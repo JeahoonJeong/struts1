3071fd374100b632f6d85d784e7d2e09c8426d25
Unknown <JeahoonJeong789@gmail.com>
2018-08-23 11:39:59 +0900

    struts1 프로젝트를 위한 db, file, encoding, paging 관련 클래스 추가

---
+++ src/com/util/filter/CharacterEncodingFilter.java
@@ -0,0 +1,46 @@
+package com.util.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class CharacterEncodingFilter implements Filter {
+	private String charset; 
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		charset = config.getInitParameter("charset");
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+		
+		if(req instanceof HttpServletRequest) {
+			HttpServletRequest request = (HttpServletRequest)req;
+			String uri = request.getRequestURI();
+			
+			if(request.getMethod().equalsIgnoreCase("POST")) {
+				if(uri.indexOf("ajax.do") != -1)
+					request.setCharacterEncoding("utf-8");
+				else
+					request.setCharacterEncoding(charset);
+			}
+		}
+		
+		// 다음 체인
+		// 없을 경우 request가 서버로 전달되지 않는다.
+		chain.doFilter(req, resp);
+		
+	}
+	@Override
+	public void destroy() {
+	}
+
+}
